a15fbb9a243597b42e6559fcb2e94c04f9076b8c
Rafael Rozon <rafaelrozon.developer@gmail.com>
2020-11-28 10:59:34 -0800

    chore(config): add husky, repo config in package.json

--- utils/runScript.js
+++ utils/runScript.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cp = require('child_process');
 
 function runScript(scriptFile) {
-    cp.exec(path.resolve(__dirname, `../scripts/${scriptFile}`), (error, stdout, stderr) => {
+    cp.execFile(path.resolve(__dirname, `../scripts/${scriptFile}`), (error, stdout, stderr) => {
         if (error) {
             console.log('runScript/error: ', error);
         }
